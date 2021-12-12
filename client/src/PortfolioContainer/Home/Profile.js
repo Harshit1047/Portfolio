00d9af0fa6f716a5a6213f47beb48459180b48e2
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-12-12 12:28:11 +0530

    Changes

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='colz'>
+                    <a href='https://www.facebook.com/'>
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href='https://www.facebook.com/'>
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href='https://www.facebook.com/'>
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href='https://www.facebook.com/'>
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    )
+}
