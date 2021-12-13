a3267f9bdab8d149e9c73526ad091a1646c3f8b5
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-12-13 16:02:56 +0530

    Resume+Hire me section added

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,24 +1,62 @@
 import React from 'react'
+import Typical from 'react-typical'
 export default function Profile() {
     return (
         <div className='profile-container'>
             <div className='profile-parent'>
-                <div className='colz'>
-                    <a href='https://www.facebook.com/'>
-                        <i class="fa fa-facebook-square"></i>
-                    </a>
-                    <a href='https://www.facebook.com/'>
-                        <i class="fa fa-facebook-square"></i>
-                    </a>
-                    <a href='https://www.facebook.com/'>
-                        <i class="fa fa-facebook-square"></i>
-                    </a>
-                    <a href='https://www.facebook.com/'>
-                        <i class="fa fa-facebook-square"></i>
-                    </a>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='https://www.linkedin.com/in/harshit-pratap-singh-36258a1b1/'>
+                            <i class="fa fa-linkedin-square"></i>
+                        </a>
+                        <a href='harshit79055@gmail.com'>
+                            <i class="fa fa-google"></i>
+                        </a>
+                        <a href='https://github.com/Harshit1047'>
+                            <i class="fa fa-github" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {" "}
+                            Hello, I'm <span className='highlighted-text'>Harshit</span>
+                        </span>
+                    </div>
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical
+                                loop={Infinity}
+                                steps={[
+                                    "Full Stack Web Developer",
+                                    1000,
+                                    "Java Developer",
+                                    1000,
+                                    "MERN Stack Developer",
+                                    1000,
+
+                                ]}
+                                />
+                            </h1>
+                            <span className='profile-role-tagline'>
+                            Creating servers and databases for functionality.
+
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-optins'>
+                        <button className='btn primary-btn'>
+                            {""}
+                            Hire Me{" "}
+                        </button>
+                        <a href="Resume.pdf" download='Harshit Resume.pdf'>
+                        <button className='btn high'>Get Resume</button></a>
+                   
+                    </div>
                 </div>
             </div>
         </div>
-
     )
 }
