1b5e406eff71415b4a6b325ff50e8fcebb824aa4
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-12-17 18:39:34 +0530

    parralax property added

--- client/src/PortfolioContainer/Home/Home.js
+++ client/src/PortfolioContainer/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import Header from "./Header/Header"
 import Profile from "./Profile/Profile"
 import Footer from "./Footer/Footer"
 import './Home.css'
@@ -7,7 +7,7 @@ export default function Home()
 {
     return (
         <div className="Home-contanier">
-           
+            <Header/>
             <Profile/>
             <Footer/>
         
